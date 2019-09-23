afb4c3e3940cdf0f8925996feb26a14bce7f3e23
Mario Garcia Pinan <jaitoone@gmail.com>
2019-09-23 13:38:22 +0100

    User can add or withdraw money from account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,4 +5,11 @@ class BankAccount
     @balance = 0
   end
 
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+  end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,7 +1,20 @@
 require 'bank_account'
 
 describe BankAccount do
+
   it 'starts with a balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'can deposit money in balance' do
+    subject.deposit(1000)
+    expect(subject.balance).to be 1000
+  end
+
+  it 'can withdraw money from balance' do
+    subject.deposit(1000)
+    subject.withdraw(500)
+    expect(subject.balance).to be 500
+  end
+  
 end
